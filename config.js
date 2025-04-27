1aaa8d901a67b1ddfde84dcb274779c1c41256ba
KavindaKiridana <kavindakiridena@gmail.com>
2025-04-27 22:03:00 +0530

    editing

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyBlfnZvS9N2XBjBMiKMnlCywc-FjBnkDhQ",
+    authDomain: "plnat-growth.firebaseapp.com",
+    databaseURL: "https://plnat-growth-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "plnat-growth",
+    storageBucket: "plnat-growth.appspot.com",
+    messagingSenderId: "714741844786",
+    appId: "1:714741844786:web:ce7246947c8d5a109f252b",
+    measurementId: "G-PLPRSHRBH7"
+};
