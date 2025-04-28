ecea4e7e5a7f624631e266bde72c6c6084498139
KavindaKiridana <kavindakiridena@gmail.com>
2025-04-28 11:02:58 +0530

    bug fixed

--- a/vite.config.js
+++ a/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/smart-green-house/',   // 👈 Keep your base path for GitHub Pages
+    base: '/smart-green-house/',
     build: {
-        target: 'esnext'              // 👈 Add target to fix "top-level await" error
+        target: 'esnext'
     }
 })
